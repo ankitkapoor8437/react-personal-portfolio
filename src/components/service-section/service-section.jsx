add73792a09d8786864c2eae51af41b1e396ef6d
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-07-20 10:33:13 +0530

    made some changes in Service section.

--- src/components/service-section/service-section.jsx
+++ src/components/service-section/service-section.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import '../service-section/service-section.css';
 
+const services = [
+    {
+        "icon": "fa-solid fa-code",
+        "title": "Web Design",
+        "description": "As a web developer, I possess a range of technical skills necessary to build and maintain websites. I have expertise in HTML, CSS, and JavaScript, which allows me to create engaging and interactive web pages. My knowledge of CSS frameworks like Bootstrap and Tailwind CSS enables me to create engaging designs efficiently.",
+        "learnMoreLink": ""
+    },
+    {
+        "icon": "fa-solid fa-crop",
+        "title": "UX UI",
+        "description": "I possess a strong understanding of user-centered design principles and have experience in creating intuitive and visually appealing user interfaces for various digital platforms. My proficiency in design tools like Sketch, Figma, and Adobe Creative Suite enables me to create wireframes, prototypes, and high-fidelity mockups efficiently. I also have experience in conducting user research, usability testing, and iterating designs based on user feedback.",
+        "learnMoreLink": ""
+    },
+    {
+        "icon": "fa-brands fa-app-store",
+        "title": "Frameworks",
+        "description": "I have experience in using several web frameworks, including Django, Flask, and Express.js. I am proficient in building RESTful APIs, handling database operations, and implementing user authentication and authorization using these frameworks. Additionally, I have a good understanding of frontend frameworks like React, enabling me to create full-stack web applications.",
+        "learnMoreLink": ""
+    }
+]
 
 
 const ServiceSection = () => {
-    return(
+    return (
         // <!-- Services Section -->
         <div id="services">
             <div className="container">
                 <h1 className="subTitle">My Expertise</h1>
                 <div className="services-list">
-                    <div>
+
+                    {services.map(({ icon, title, description, learnMoreLink }) => (<div>
+                        <i className={icon}></i>
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                        <br />
+                        <a href="" className="btn btn2" >Learn More</a>
+                    </div>))}
+
+                    {/* <div>
                         <i className="fa-solid fa-code"></i>
                         <h2>Web Design</h2>
                         <p>As a web developer, I possess a range of technical skills necessary to build and maintain
@@ -39,12 +68,12 @@ const ServiceSection = () => {
                             understanding of frontend frameworks like React, enabling me to create full-stack
                             web applications.</p>
                         <a href="">Learn More</a>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
     );
-} 
+}
 
 
 export default ServiceSection;
---
+++ src/data-json/services.js
@@ -0,0 +1,22 @@
+const Services = [
+    {
+      "icon": "fa-solid fa-code",
+      "title": "Web Design",
+      "description": "As a web developer, I possess a range of technical skills necessary to build and maintain websites. I have expertise in HTML, CSS, and JavaScript, which allows me to create engaging and interactive web pages. My knowledge of CSS frameworks like Bootstrap and Tailwind CSS enables me to create engaging designs efficiently.",
+      "learnMoreLink": ""
+    },
+    {
+      "icon": "fa-solid fa-crop",
+      "title": "UX UI",
+      "description": "I possess a strong understanding of user-centered design principles and have experience in creating intuitive and visually appealing user interfaces for various digital platforms. My proficiency in design tools like Sketch, Figma, and Adobe Creative Suite enables me to create wireframes, prototypes, and high-fidelity mockups efficiently. I also have experience in conducting user research, usability testing, and iterating designs based on user feedback.",
+      "learnMoreLink": ""
+    },
+    {
+      "icon": "fa-brands fa-app-store",
+      "title": "Frameworks",
+      "description": "I have experience in using several web frameworks, including Django, Flask, and Express.js. I am proficient in building RESTful APIs, handling database operations, and implementing user authentication and authorization using these frameworks. Additionally, I have a good understanding of frontend frameworks like React, enabling me to create full-stack web applications.",
+      "learnMoreLink": ""
+    }
+  ]
+
+  export default Services;
